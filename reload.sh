b3772affe1353a9c5e4575785880899950997ee8
Saikrishna <saikrishna.edupuganti@intel.com>
2019-01-11 15:22:47 -0800

    Initial commit
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>

---
+++ reload.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+docker exec bess /opt/bessctl/bessctl/bessctl daemon reset
+docker exec bess /opt/bessctl/bessctl/bessctl run file /conf/router.bess
+docker exec bess /opt/bessctl/bessctl/bessctl show pipeline
