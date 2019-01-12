91654838c5a26291effd0888f62e250f3ab25e92
Saikrishna <saikrishna.edupuganti@intel.com>
2019-01-12 11:05:21 -0800

    Error handling

--- reload.sh
+++ reload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-docker exec bess /opt/bessctl/bessctl/bessctl daemon reset
-docker exec bess /opt/bessctl/bessctl/bessctl run file /conf/router.bess
-docker exec bess /opt/bessctl/bessctl/bessctl show pipeline | tee pipeline.txt
+BESSCTL="/opt/bessctl/bessctl/bessctl"
+# IFNAME is veth created in setup.sh
+docker exec -e IFNAME=foo bess $BESSCTL daemon reset -- run file /conf/router.bess
+docker exec bess $BESSCTL show pipeline | tee pipeline.txt
