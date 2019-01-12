018c3605f7797bf0bcb379d00eb9b5a6dcfb3c3b
Saikrishna <saikrishna.edupuganti@intel.com>
2019-01-11 17:31:24 -0800

    Add output of show pipeline
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>

--- reload.sh
+++ reload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker exec bess /opt/bessctl/bessctl/bessctl daemon reset
 docker exec bess /opt/bessctl/bessctl/bessctl run file /conf/router.bess
-docker exec bess /opt/bessctl/bessctl/bessctl show pipeline
+docker exec bess /opt/bessctl/bessctl/bessctl show pipeline | tee pipeline.txt
