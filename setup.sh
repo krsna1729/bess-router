81e76eb91b6ab2628bf82b217889db5eb9416897
Saikrishna <saikrishna.edupuganti@intel.com>
2019-01-11 17:26:46 -0800

    Refactor bess conf and add setup script
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+docker stop bess
+
+docker run --name bess -itd --rm --cap-add NET_ADMIN \
+--device=/dev/vfio/48 --device=/dev/vfio/49 --device=/dev/vfio/vfio \
+--ulimit memlock=-1 -v /dev/hugepages:/dev/hugepages \
+-v $(pwd):/conf \
+ngick8stesting/ngic-bessd-ctl:pkgs bessd -f
+
+docker exec bess bash -c "
+ip link add foo type veth peer name foo-vdev;
+ip link set foo up;
+ip link set foo-vdev up;
+ip addr add 198.18.0.1/30 dev foo;
+ip route;
+"
